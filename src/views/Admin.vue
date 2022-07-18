6b4623d416ebd88bdfa568bfc00efb4ef5b9782d
TranTam <tamvaa1306@gmail.com>
2022-07-18 17:57:55 +0700

    admin UI

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,12 +1,79 @@
 <template>
   <div class="admin">
-    <h1>Admin</h1>
+    <div class="container">
+      <h2>Administration</h2>
+      <div class="admin-info">
+        <h2>Add Admin</h2>
+        <div class="input">
+          <input
+            type="text"
+            placeholder="Enter user email to make them an admin"
+            id="addAdmins"
+            v-model="adminEmail"
+          />
+        </div>
+        <span>{{ this.functionMsg }}</span>
+        <button class="button">Submit</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      adminEmail: "",
+      functionMsg: null,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.admin {
+  .container {
+    max-width: 1000px;
+    padding: 60px 25px;
+    h2 {
+      text-align: center;
+      margin-bottom: 16px;
+      font-weight: 300;
+      font-size: 32px;
+    }
+    .admin-info {
+      border-radius: 8px;
+      box-shadow: 0 4px 6px -1px rgba($color: #000000, $alpha: 0.1),
+        0 2px 4px -1px rgba($color: #000000, $alpha: 0.06);
+      padding: 32px;
+      background: #f1f1f1;
+      display: flex;
+      flex-direction: column;
+      max-width: 600px;
+      margin: 32px auto;
+      span {
+        font-size: 14px;
+      }
+      .input {
+        margin: 16px 0;
+        input {
+          width: 100%;
+          border: none;
+          background: #f2f7f6;
+          padding: 8px;
+          height: 50px;
+          outline: none;
+          @media (max-width: 900px) {
+          }
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+      button {
+        align-self: center;
+      }
+    }
+  }
+}
 </style>
